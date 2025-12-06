7c0a9b89714268881a43a2eafd2abc88e660d3f2
ssoumenmaity3 <sm8039912@gmail.com>
2025-12-06 10:25:55 +0530

    dagshub

--- app.py
+++ app.py
@@ -95,8 +95,11 @@ if __name__ == "__main__":
             # There are other ways to use the Model Registry, which depends on the use case,
             # please refer to the doc for more information:
             # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(
-                lr, "model", registered_model_name="ElasticnetWineModel"
-            )
+
+            # mlflow.sklearn.log_model(
+            #     lr, "model", registered_model_name="ElasticnetWineModel"
+            # )
+            mlflow.sklearn.log_model(lr, "model")
+
         else:
             mlflow.sklearn.log_model(lr, "model")
